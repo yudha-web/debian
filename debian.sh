ad6c0a1a05e13920245f8a249bfabfcaec5b2027
yudha-web <faisalyudha738@gmail.com>
2025-02-12 09:52:31 +0700

    debian.sh

--- debian.sh
+++ debian.sh
@@ -4,9 +4,9 @@
 echo "Memperbarui sistem..."
 apt update -y && apt upgrade -y
 
-# Instal Apache2, PHP, MariaDB, phpMyAdmin, dan SSH
-echo "Menginstal Apache2, PHP, MariaDB, phpMyAdmin, dan SSH..."
-apt install apache2 php libapache2-mod-php php-mysql php-cli php-zip php-xml php-mbstring mariadb-server mariadb-client phpmyadmin openssh-server -y
+# Instal Apache2, PHP, MariaDB, phpMyAdmin, SSH, dan WordPress
+echo "Menginstal Apache2, PHP, MariaDB, phpMyAdmin, SSH, dan WordPress..."
+apt install apache2 php libapache2-mod-php php-mysql php-cli php-zip php-xml php-mbstring mariadb-server mariadb-client phpmyadmin openssh-server wget unzip -y
 
 # Aktifkan dan mulai layanan
 echo "Mengaktifkan dan memulai layanan..."
@@ -40,6 +40,34 @@ echo "\$cfg['Servers'][\$i]['password'] = '$db_pass';" >> /usr/share/phpmyadmin/
 echo "\$cfg['Servers'][\$i]['host'] = 'localhost';" >> /usr/share/phpmyadmin/config.inc.php
 echo "\$cfg['Servers'][\$i]['AllowNoPassword'] = FALSE;" >> /usr/share/phpmyadmin/config.inc.php
 
+# Unduh dan pasang WordPress
+echo "Mengunduh dan memasang WordPress..."
+cd /var/www/html
+wget https://wordpress.org/latest.tar.gz
+tar -xvzf latest.tar.gz
+rm latest.tar.gz
+
+# Atur izin direktori WordPress menjadi 777
+echo "Mengatur izin direktori WordPress menjadi 777..."
+chown -R www-data:www-data /var/www/html/wordpress
+chmod -R 777 /var/www/html/wordpress
+
+# Konfigurasi database WordPress
+echo "Mengonfigurasi database WordPress..."
+echo "Masukkan nama database untuk WordPress:"
+read wp_db
+mysql -e "CREATE DATABASE $wp_db;"
+mysql -e "GRANT ALL PRIVILEGES ON $wp_db.* TO '$db_user'@'localhost' IDENTIFIED BY '$db_pass';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Salin file konfigurasi WordPress
+cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+
+# Edit wp-config.php untuk menambahkan informasi database
+sed -i "s/database_name_here/$wp_db/" /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$db_user/" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$db_pass/" /var/www/html/wordpress/wp-config.php
+
 # Restart Apache2 untuk menerapkan konfigurasi
 echo "Merestart Apache2..."
 systemctl restart apache2
@@ -47,10 +75,12 @@ systemctl restart apache2
 # Informasi Akses
 echo "Instalasi selesai!"
 echo "Akses phpMyAdmin di http://<your_server_ip>/phpmyadmin"
+echo "Akses WordPress di http://<your_server_ip>/wordpress"
 echo "Gantilah <your_server_ip> dengan alamat IP server Anda."
 echo ""
 echo "Terima kasih telah menggunakan script ini!"
 echo "Nama pengguna MariaDB: $db_user"
 echo "Password MariaDB: $db_pass"
+echo "Nama database WordPress: $wp_db"
 echo ""
 echo "Watermark: makan"
