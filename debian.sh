61b5f66e5123b2a5fdb1e1acb74293840b7f78fa
Yudha <faisalyudha738@gmail.com>
2025-02-20 08:45:25 +0700

    debian.sh

--- debian.sh
+++ debian.sh
@@ -4,30 +4,26 @@ Warna teks
 
 GREEN="\e[32m" YELLOW="\e[33m" CYAN="\e[36m" RESET="\e[0m"
 
-Memeriksa dan menginstal pv untuk progress bar
-
-if ! command -v pv &> /dev/null; then echo -e "${YELLOW}Menginstal pv untuk progress bar...${RESET}" apt install -y pv fi
-
 Memperbarui sistem
 
-echo -e "${YELLOW}Memperbarui sistem...${RESET}" apt update -y | pv -lep -s 100 > /dev/null apt upgrade -y | pv -lep -s 100 > /dev/null
+echo -e "${YELLOW}Memperbarui sistem...${RESET}" apt update -y && apt upgrade -y
 
 Menginstal paket yang diperlukan
 
-echo -e "${YELLOW}Menginstal Apache2, PHP, MariaDB, phpMyAdmin, SSH, dan WordPress...${RESET}" echo -e "Harap tunggu..." apt install -y apache2 php libapache2-mod-php php-mysql php-cli php-zip php-xml php-mbstring 
-mariadb-server mariadb-client openssh-server wget unzip | pv -lep -s 100 > /dev/null
+echo -e "${YELLOW}Menginstal Apache2, PHP, MariaDB, phpMyAdmin, SSH, dan WordPress...${RESET}" apt install -y apache2 php libapache2-mod-php php-mysql php-cli php-zip php-xml php-mbstring 
+mariadb-server mariadb-client openssh-server wget unzip
 
 Konfigurasi otomatis phpMyAdmin
 
-echo -e "${CYAN}Mengonfigurasi phpMyAdmin secara otomatis...${RESET}" echo -e "${YELLOW}Masukkan password admin phpMyAdmin:${RESET}" read -s phpmyadmin_pass debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true" debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $phpmyadmin_pass" debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $phpmyadmin_pass"
+echo -e "${CYAN}Mengonfigurasi phpMyAdmin secara otomatis...${RESET}" debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true" debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password root123" debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password root123"
 
 Instal phpMyAdmin
 
-apt install -y phpmyadmin | pv -lep -s 100 > /dev/null
+apt install -y phpmyadmin
 
 Mengaktifkan dan memulai layanan
 
-echo -e "${CYAN}Mengaktifkan layanan...${RESET}" systemctl enable apache2 mariadb ssh | pv -lep -s 100 > /dev/null systemctl start apache2 mariadb ssh | pv -lep -s 100 > /dev/null
+echo -e "${CYAN}Mengaktifkan layanan...${RESET}" systemctl enable apache2 mariadb ssh systemctl start apache2 mariadb ssh
 
 Konfigurasi SSH agar root bisa login
 
@@ -43,7 +39,7 @@ echo -e "${CYAN}Mengonfigurasi MariaDB...${RESET}" mysql -e "CREATE DATABASE $wp
 
 Mengunduh dan memasang WordPress
 
-echo -e "${CYAN}Mengunduh dan memasang WordPress...${RESET}" cd /var/www/html wget https://wordpress.org/latest.tar.gz | pv -lep -s 100 > /dev/null tar -xvzf latest.tar.gz | pv -lep -s 100 > /dev/null rm latest.tar.gz
+echo -e "${CYAN}Mengunduh dan memasang WordPress...${RESET}" cd /var/www/html wget https://wordpress.org/latest.tar.gz tar -xvzf latest.tar.gz rm latest.tar.gz
 
 Mengatur izin direktori WordPress
 
@@ -53,23 +49,21 @@ Mengonfigurasi wp-config.php
 
 cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php sed -i "s/database_name_here/$wp_db/" /var/www/html/wordpress/wp-config.php sed -i "s/username_here/$db_user/" /var/www/html/wordpress/wp-config.php sed -i "s/password_here/$db_pass/" /var/www/html/wordpress/wp-config.php
 
-Mengatur bahasa WordPress ke Indonesia secara otomatis
+Mengatur bahasa WordPress ke Indonesia
 
-echo -e "${YELLOW}Mengatur bahasa WordPress ke Indonesia...${RESET}" wget -P /var/www/html/wordpress/wp-content/languages https://downloads.wordpress.org/translation/core/6.4/id_ID.zip | pv -lep -s 100 > /dev/null unzip /var/www/html/wordpress/wp-content/languages/id_ID.zip -d /var/www/html/wordpress/wp-content/languages | pv -lep -s 100 > /dev/null rm /var/www/html/wordpress/wp-content/languages/id_ID.zip chown -R www-data:www-data /var/www/html/wordpress/wp-content/languages
-
-mysql -e "USE $wp_db; UPDATE wp_options SET option_value='id_ID' WHERE option_name='WPLANG';"
+echo -e "${YELLOW}Menetapkan bahasa WordPress ke Indonesia...${RESET}" sed -i "s/define('WPLANG', '');/define('WPLANG', 'id_ID');/" /var/www/html/wordpress/wp-config.php
 
 Menambahkan watermark unik
 
 echo -e "${CYAN}Menambahkan watermark...${RESET}" cat <<EOL >> /var/www/html/wordpress/wp-config.php
 
-/* === Watermark by makan bang === / / Rajin sekolah tiap hari, / / Tapi ngantuk pas pelajaran. / / PR lupa belum dikerjain, / / Senyum aja biar aman! */
+/* === Watermark by makan bang === / / Skrip ini dibuat oleh makan bang, dilarang dicuri! */
 
 EOL
 
 Merestart Apache untuk menerapkan perubahan
 
-echo -e "${YELLOW}Merestart Apache2...${RESET}" systemctl restart apache2 | pv -lep -s 100 > /dev/null
+echo -e "${YELLOW}Merestart Apache2...${RESET}" systemctl restart apache2
 
 Mendapatkan IP server
 
@@ -79,7 +73,7 @@ Informasi Akses
 
 echo -e "${GREEN}==========================================${RESET}" echo -e "${GREEN}Instalasi selesai!${RESET}" echo -e "Akses phpMyAdmin di: ${CYAN}http://$server_ip/phpmyadmin${RESET}" echo -e "Akses WordPress di: ${CYAN}http://$server_ip/wordpress${RESET}" echo -e "${GREEN}==========================================${RESET}" echo -e "${CYAN}Nama pengguna MariaDB: $db_user${RESET}" echo -e "${CYAN}Password MariaDB: $db_pass${RESET}" echo -e "${CYAN}Nama database WordPress: $wp_db${RESET}" echo -e "${GREEN}==========================================${RESET}"
 
-Kucing ASCII sedang makan
+Kucing ASCII
 
-echo -e "${YELLOW}Terima kasih telah menggunakan skrip ini, makan bang!${RESET}" echo -e "${CYAN}" echo " /_/\   (=^･ω･^=) 🍗" echo "( o.o )   makan bang!" echo " > 🍣 <  " echo -e "${RESET}"
+echo -e "${YELLOW}Terima kasih telah menggunakan skrip ini, makan bang!${RESET}" echo -e "${CYAN}" echo " /_/\   (=^･ω･^=)" echo "( o.o )  Meow!" echo " > 🍣 <  " echo -e "${RESET}"
 
